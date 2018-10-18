20dda4d51a4a7779524c033216b58494bb940710
idoyef <idoyef@gmail.com>
2018-10-18 22:57:10 +0300

    initial project

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div id="app">
+    <MainHeader/>
+    <div style="height: 1000px; background-color: #ededed;">
+      dgdsfgdfgdgdfgdfgdfgdfgd <br><br>
+      saadsasadas  
+    </div>    
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue';
+import MainHeader from './components/MainHeader.vue';
+
+export default {
+  name: 'app',
+  components: {
+    HelloWorld,
+    MainHeader
+  }
+}
+</script>
+
+<style>
+  html, body {
+    height:100%;
+    padding:0;
+    margin:0;
+  }
+  #app {    
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;   
+  }
+</style>
---
+++ src/components/MainHeader.vue
@@ -0,0 +1,68 @@
+<template>
+    <div class="main-header-container">
+        <img class="main-app-logo" alt="Vue logo" src="../assets/oruzLogo.png">
+
+        <div class="right-container">
+            <button class="main-header-button login-button">Login</button>
+            <button class="main-header-button join-in-button">Join</button>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'MainHeader',
+  props: {
+    
+  }
+}
+</script>
+
+<style scoped>
+    .main-header-container {
+        position: fixed;
+        width: 100%;
+        height: 80px;
+        background-color: white;       
+    }
+    
+    .main-app-logo {
+        float: left;
+        padding: 12px;
+        padding-left: 120px;        
+        height: 55px;
+        width: 140px;
+        cursor: pointer;
+    }
+
+    .right-container {
+        float: right;
+        margin: 25px 120px;
+    }
+
+    .main-header-button {
+        width: 100px;
+        height: 30px;
+        border: none;
+        border-radius: 6px;
+        cursor: pointer;
+    }
+
+    .login-button{      
+        background-color: #C5B358;
+        color: white;       
+    }
+
+    .join-in-button {              
+        margin-left: 20px;
+        color: #C5B358;
+        background-color: white;
+        border: #C5B358 1px solid;
+    }
+
+    .join-in-button:hover {       
+         background-color: #C5B358;
+         color: white;
+         transition: 1s;
+    }
+</style>
